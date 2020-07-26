4b5d2c52f1ae9c3c4e9cca8ac0eb003880d75069
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2020-07-25 23:19:30 -0300

    Documentado recurso de atualizar do funcionário
    Documentado o recurso disponibilizado para atualizar o funcionário

--- app/Http/Documentacao/Controllers/FuncionarioController.php
+++ app/Http/Documentacao/Controllers/FuncionarioController.php
@@ -26,6 +26,29 @@ final class FuncionarioController implements Documentacao {
                 ]                            
             ];       
         return $retorno;
+    }
+    public function atualizar(){
+        $retorno = [
+            'mensagem' => 'Atualiza um funcionário, o recurso atualiza o usuário e o endereço vinculado ao funcionário
+            somente o proprietário da empresa que o funcionário que está vinculado pode atualizar, é necessario enviar a
+            senha de confirmação ao atualizar um funcionário, para manter a senha atual ou modificar.',
+            'verbo' => 'put',
+            'url' => 'http://localhost:8000/api/funcionario/atualizar',
+            'json-esperado' => [
+                "id"                    => 20,
+                "name"                  => "Teste de funcionário",
+                "email"                 => "funcionario@email.com",
+                "password"              => "secret",
+                "password_confirmation" => "secret",
+                "rua"                   => "Líbano jose",
+                "numero"                =>150,
+                 "bairro"               => "Santa Luzia",
+                "complemento"           => "SN",
+                "cep"                   => "88852-536",
+                "cidade_id"             =>2                    
+                ]                            
+            ];       
+        return $retorno;
     }   
 }
 
