e00111c24d1989dd84a86f982f074bb7e30a32f7
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2020-07-29 21:25:41 -0300

    Criado funcionárioDTO
    Criado recurso que disponibiliza os funcionários

--- app/Http/Dto/FuncionarioDTO.php
+++ app/Http/Dto/FuncionarioDTO.php
@@ -12,12 +12,29 @@ use App\Exceptions\ApiException;
 
 class FuncionarioDTO
 {
+    private $usuarioService;
+    private $enderecoService;
     private $funcionarioRepository;
     public function __construct()  {
         $this->funcionarioRepository = new FuncionarioRepository();
     }
     public function obterFuncionarios($empresa){
+        $this->usuarioService = new UserService();
+        $this->enderecoService = new EnderecoService();
+        $this->empresaService = new EmpresaService();
+
+        $lista = array();
         $funcionarios = $this->funcionarioRepository->obterFuncionarios($empresa);
-        return $funcionarios;
+        foreach ($funcionarios as $funcionario) {
+            $usuario = $this->usuarioService->obterPorId($funcionario->usuario_id);
+            $dto =[
+                'name'   => $usuario->name,
+                'email'     => $usuario->email,
+                'endereco'  => $this->enderecoService->obterPorId($funcionario->endereco_id),
+                'empresa'  => $this->enderecoService->obterPorId($funcionario->endereco_id),
+            ];
+            array_push($lista, $dto);
+        }
+        return $lista;
     }
 }
